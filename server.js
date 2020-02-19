d673604fa4ca62c8aa4e6c85adef22df958c0ea4
Juan Denzer <denzer.juan@gmail.com>
2020-02-19 14:10:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
-  requireHeader: ['XMLHttpRequest', 'x-requested-with'],
+  requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
     'cookie',
